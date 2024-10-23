23cb864449638261a49841e70ee60ef43c63e6b1
MoDaORIF <david.mostoslavski@formation.orif.ch>
2024-10-23 14:36:44 +0200

    users and auth delete

--- users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'username', 'status', 'is_active', 'is_staff']
